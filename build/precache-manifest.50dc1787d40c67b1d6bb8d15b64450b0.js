62265ffca0fdacf3cfc02e345545657ec90b9504
Azfar Bilal <azfarbilal@Azfars-MacBook-Pro.local>
2020-02-11 12:40:29 +0500

    new files

---
+++ build/precache-manifest.50dc1787d40c67b1d6bb8d15b64450b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f26853aa94f9d5731eb807bcb55ddae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89c8a94dd0e8ec3ba321",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "0a090dc4a435e95d06d4",
+    "url": "/static/css/main.b18b26ee.chunk.css"
+  },
+  {
+    "revision": "89c8a94dd0e8ec3ba321",
+    "url": "/static/js/2.3c878807.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.3c878807.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a090dc4a435e95d06d4",
+    "url": "/static/js/main.b4e7409c.chunk.js"
+  },
+  {
+    "revision": "af15c0d4ce7075498753",
+    "url": "/static/js/runtime-main.1ab7c014.js"
+  }
+]);
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Rank = ({name, entries}) => {
+	return(
+		<div>
+			{`${name} , your current rank is...`}
+      <div className='white f1 '>
+        {entries}
+      </div>
+			<div className='white f1'>
+				{'#5'}
+				
+			</div>
+		</div>
+		);
+}
+
+export default Rank;
